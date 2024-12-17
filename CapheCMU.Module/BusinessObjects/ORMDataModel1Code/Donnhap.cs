75dcaf17b28e9b2486cedbe424fbf79bbfbf2544
Ngọc Thịnh <ngocthinhlapro123@gmail.com>
2024-12-18 03:28:09 +0700

    cập nhật all

---
+++ CapheCMU.Module/BusinessObjects/ORMDataModel1Code/Donnhap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace CapheCMU.Module.BusinessObjects.ORMDataModel1
+{
+
+    public partial class Donnhap
+    {
+        public Donnhap(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
